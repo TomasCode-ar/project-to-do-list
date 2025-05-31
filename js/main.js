234999dad4779d67261a3913dfd7a632465b4132
tomas <tomasdesindes1@gmail.com>
2025-05-30 21:05:26 -0300

    se agrega botones y contador

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let taskCounter = 0;
+
 let btn = document.getElementById('btn-add-task');
 
 btn.addEventListener("click", addTask);
@@ -8,12 +10,30 @@ function addTask() {
     li.className = "list-group-item d-flex justify-content-between align-items-start";
     let span = document.createElement("span");
     span.className = "mt-1";
+    let countTask = document.getElementById("countTask")
     let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "btn btn-danger";
     let taskInput = document.getElementById("taskInput"); // For task entries
     let taskList = document.getElementById("taskList"); // For listing tasks
 
     if (taskInput.value == "") {
-        alert("Add a task");
+        alert("Please, write a task!");
+        taskInput.focus();
         return;
     }
+
+    span.textContent = taskInput.value; // Lo que esta en la caja de entrada pasa al span
+    li.appendChild(span); //Agrego el span dentro del li
+    taskCounter++;
+    li.appendChild(deleteButton);
+    countTask.textContent = "Number of tasks : " + taskCounter;
+    taskList.appendChild(li);
+    taskInput.value = "";
+    taskInput.focus();
+    deleteButton.addEventListener('click', function () {
+        taskCounter--;
+        countTask.textContent = "Number of tasks : " + taskCounter;
+         taskList.removeChild(li);
+    });
 }
